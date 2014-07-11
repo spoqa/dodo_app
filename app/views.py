5e4110849f1bb3f2fc2c897573918abda90d89b1
ung <qooraven@gmail.com>
2014-07-11 11:10:10 +0900

    testing for db_session.add()

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -9,17 +9,44 @@ from app.models import LMS, SMS
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cur = db_session.query(LMS)
+    cur2 = db_session.query(SMS)
+    List = [dict(id=lms.mt_pr, subject=lms.subject,
+                 content=lms.content, callback=lms.callback,
+                 recipent_num=lms.recipent_num) for lms in cur]
+    List2 = [dict(id=sms.mt_pr, content=sms.content,
+                  callback=sms.callback,
+                  recipent_num=sms.recipent_num) for sms in cur2]
+    import pdb; pdb.set_trace()
+    return render_template('index.html', List=List, List2=List2)
 
 
-@app.route('/messages', methods=['GET', 'POST'])
+@app.route('/messages', methods=['GET'])
 def messages():
-    if len(request.form['msg'].encode('euc-kr')) >= 80:
-        msg = LMS(request.form['title'], request.form['msg'],
-                  request.form['phone_from'], request.form['phone'])
+    title = request.args['title']
+    msg = request.args['msg']
+    phone_from = request.args['phone_from']
+    phone = request.args['phone']
+    import pdb; pdb.set_trace()
+    try:
+        int(phone_from)
+        int(phone)
+        resp = 'Success'
+        if len(title) < 1:
+            resp = 'Message is Null'
+            raise Exception(ValueError)
+    except ValueError:
+        resp = 'form phone_from or phone is not integer'
+        """
     else:
-        msg = SMS(request.form['msg'], request.form['phone_from'],
-                  request.form['phone'])
-    db_session.add(msg)
-    db_session.commit()
-    return redirect(url_for('index'))
+        if len(msg.encode('euc-kr')) >= 80:
+            db_msg = LMS(title,msg, phone_from, phone)
+        else:
+            db_msg = SMS(msg, phone_from, phone)
+        db_session.add(db_msg)
+        db_session.commit()
+        """
+    finally:
+        return jsonify(result=resp)
+
+
